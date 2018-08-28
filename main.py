25542e9b1546f8c05436875894d2468e02021ef7
Kostas Makedos <kostas.makedos@gmail.com>
2018-08-28 15:04:28 +0300

    Add multiprocessing with half number of CPUs from system

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import sys
 import os
-
-
-NUM_WORKERS = 4
 from PDF import PDF
-def check_dir(path):
+import time
+import multiprocessing
+
+def find_pdf_files(path):
+    pdfs = []
     for dirpath, dnames, fnames in os.walk(path):
         for f in fnames:
-
             if f.endswith("pdf"):
-                pdf = PDF(os.path.join(dirpath, f))
-                title = pdf.get_title()
-                print(title)
+                pdf_filepath = os.path.join(dirpath, f)
+                pdfs.append(pdf_filepath)
+    return pdfs
+def get_pdf_title(pdf_filepath):
+    pdf = PDF(pdf_filepath)
+    print(pdf.get_title())
 
-            else:
-                print("File is not pdf ", file=sys.stderr)
+
+
+
 if __name__ == "__main__":
+    start_time = time.time()
+    NUM_WORKERS = int(multiprocessing.cpu_count() / 2)
+    print("Starting with %d workers" % NUM_WORKERS)
     if len(sys.argv) > 1:
         path = sys.argv[1]
     else:
         path = "/space/Training + Ebooks/"
-        check_dir(path)
+        pdfs = find_pdf_files(path)
+        pool = multiprocessing.Pool(processes=NUM_WORKERS)
+        mp = pool.map_async(get_pdf_title, pdfs)
+        mp.wait()
+    end_time = time.time()
+    print("Process finished in %ssecs using %d workers" %((end_time - start_time), NUM_WORKERS))

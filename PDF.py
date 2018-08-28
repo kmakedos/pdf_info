cfaaca5975d185b72b7f52399ebf654a17afd946
Kostas Makedos <kostas.makedos@gmail.com>
2018-08-28 14:40:35 +0300

    Traversing folders and read pdf titles from files

--- PDF.py
+++ PDF.py
@@ -1,12 +1,36 @@
 import PyPDF2
+import os
+import sys
+from PyPDF2.utils import PdfReadError,PdfStreamError
+
 
 class PDF(object):
-    def __init__(self, pdf_file):
+    def __init__(self, pdf_filename):
         try:
-            self.pdf_file = open(pdf_file, 'rb')
+            self.pdf_filename = pdf_filename
+            self.pdf_file = open(self.pdf_filename, 'rb')
             self.pdf_reader = PyPDF2.PdfFileReader(self.pdf_file)
-        except (PyPDF2.PdfReadError, IOError) as e:
-            print(e.strerror)
+        except (IOError) as e:
+            print(e, file=sys.stderr)
+        except (PdfReadError, PdfStreamError, ValueError,AttributeError, TypeError) as e:
+            print(e, file=sys.stderr)
+
+
+
+    def get_title(self):
+        title = None
+        try:
+            title = self.pdf_reader.getDocumentInfo().title
+            # If title is not set in file, set as title the filename without pdf extension
+            if not title:
+                title = os.path.splitext(os.path.basename(self.pdf_filename))[0]
+        except (IOError,ValueError,AttributeError) as e:
+            print(e, file=sys.stderr)
+        except (PdfReadError) as e:
+            print("Cannot read title from file (maybe encrypted?): ", self.pdf_filename, file = sys.stderr)
+        return title
+
+
     def get_page_text(self, page):
         self.pdf_page = self.pdf_reader.getPage(page)
         return self.pdf_page.extractText()
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import sys
 import os
+
+
+NUM_WORKERS = 4
 from PDF import PDF
+def check_dir(path):
+    for dirpath, dnames, fnames in os.walk(path):
+        for f in fnames:
+
+            if f.endswith("pdf"):
+                pdf = PDF(os.path.join(dirpath, f))
+                title = pdf.get_title()
+                print(title)
+
+            else:
+                print("File is not pdf ", file=sys.stderr)
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         path = sys.argv[1]
     else:
-        path = "/home/kostas/Downloads"
-    for dirpath,dnames,fnames in os.walk(path):
-        for f in fnames:
-            if f.endswith("pdf"):
-                pdf = PDF(os.path.join(dirpath, f))
-                print(pdf.get_page_text(0))
-
+        path = "/space/Training + Ebooks/"
+        check_dir(path)
